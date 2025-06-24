df5c7fc1c076b122ed07540bc7edd9f45393f477
kachakaran6 <kachakaran06@gmail.com>
2025-06-24 09:19:32 +0530

    logo and color added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Fix IPO Tracker",
+  title: "IPO Tracker",
   description: "Manage and track your Fixed IPO applications",
 };
 
@@ -16,6 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#0A84FF" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://img.icons8.com/?size=100&id=2BJx5ZZ27N9y&format=png&color=000000"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
